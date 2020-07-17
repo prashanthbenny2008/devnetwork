b6a83e9a7942f49f08ba4919fe9bdce76f52eb3b
Prashanth Benny <prashanthbenny2008@gmail.com>
2020-07-17 11:51:37 +0530

    accessing post data

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 //@route  POST api/users
-//@desc   Test route
+//@desc   Register user
 //@access Public
-router.post('/', (req, res) => res.send('User route'));
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('User route')
+});
 
 module.exports = router;
