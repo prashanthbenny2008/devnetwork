71d4080453bc3662729b228e14dd054b76b604c7
Prashanth Benny <prashanthbenny2008@gmail.com>
2021-03-06 15:28:22 +0530

    created link to dashboard from navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,10 +9,18 @@ const Navbar = ({ auth : { isAuthenticated, loading }, logout }) => {
 
   const authLinks = (
     <ul>
-      <a onClick={logout} href="#!">
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />
+          <span className="hide-sm"> Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
           <i className="fas fa-sign-out-alt" /> 
-          <span className="hide-sm">Logout</span>
+          <span className="hide-sm"> Logout</span>
         </a>
+      </li>
     </ul>
   );
 
