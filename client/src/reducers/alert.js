2280370428bb3dba11b750007d7e26d796d842ce
Prashanth Benny <prashanthbenny2008@gmail.com>
2021-03-13 14:54:52 +0530

    clear warnings

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
 
-export default function( state = initialState, action ) {
+export default function Alert( state = initialState, action ) {
     const { type, payload } = action;
     switch(type){
         case SET_ALERT :
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -8,7 +8,7 @@ const initialState = {
     error: {}
 }
 
-export default function( state = initialState, action ) {
+export default function Profile( state = initialState, action ) {
     const { type, payload } = action;
     switch(type) {
         case GET_PROFILE:
