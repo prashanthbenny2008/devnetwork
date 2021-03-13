ec7d73f14f9f9c88f85e4ac787da636ea5878c7e
Prashanth Benny <prashanthbenny2008@gmail.com>
2021-03-13 11:35:41 +0530

    Get profile/List/github_repo action

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "../actions/types";
+import { CLEAR_PROFILE, GET_PROFILE, GET_PROFILES, GET_REPOS, PROFILE_ERROR, UPDATE_PROFILE } from "../actions/types";
 
 const initialState = {
     profile : null,
@@ -18,6 +18,12 @@ export default function( state = initialState, action ) {
                 profile: payload,
                 loading: false
             };
+        case GET_PROFILES:
+            return {
+                ...state,
+                profiles: payload,
+                loading: false
+            };
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -31,6 +37,12 @@ export default function( state = initialState, action ) {
                 repos: [],
                 loading: false
             };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: payload,
+                loading: false
+            }
         default: 
             return state;
     }
