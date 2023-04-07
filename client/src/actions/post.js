e08b58f193caa40ade68de380b51fa953a672b15
Prashanth <prashanthbenny2008@github.com>
2023-04-08 01:18:08 +0530

    error fixes

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -166,7 +166,7 @@ export const getPosts = () => async dispatch => {
  export const deleteComment = ( postId, commentId ) => async dispatch => {
 
     try {
-        const res = await axios.delete(`/api/posts/comment/${ postId }/${ commentId }`);
+        await axios.delete(`/api/posts/comment/${ postId }/${ commentId }`);
         dispatch({
             type: REMOVE_COMMENT,
             payload: commentId
